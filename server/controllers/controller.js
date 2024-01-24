74f47b362a0015271aab97bccc8d1bf1a8103c06
jackolampu <jackolampu@gmail.com>
2024-01-25 01:49:13 +0800

    add about

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,19 +1,17 @@
-require('../models/db');
+require("../models/db");
 //const Category = require('../models/Category');
-const Menu = require('../models/menu');
+const Menu = require("../models/menu");
 
 /**
  * GET /
- * Homepage 
-*/
-exports.homepage = async(req, res) => {
-    try {
-      
-      const menu = await Menu.find();
-      
-      
-      res.render('home' , { title: 'Cexec Cofee - Home', menu });
-    } catch (error) {
-      res.status(500).send({message: error.message || "Error Occured" });
-    }
+ * Homepage
+ */
+exports.homepage = async (req, res) => {
+  try {
+    const menu = await Menu.find();
+
+    res.render("home", { title: "Cexec Cofee - Home", menu });
+  } catch (error) {
+    res.status(500).send({ message: error.message || "Error Occured" });
   }
+};
